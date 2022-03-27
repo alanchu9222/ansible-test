08e0b2e5a33a898b2ba8d3d117569fd32e6a5934
unknown <alan_chu@yahoo.com>
2022-03-27 20:33:06 +1100

    'app.js'

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-app.post('/', function(req, res){
-    try {
-        console.log('executing deployment...');
-        exec("ansible-pull -U git@github.com:<GitHubUser>/<repo-name>.git <playbook>.yml", (error, stdout, stderr) => {
-                if (error) {
-                console.log(`error: ${error.message}`);
-                return;
+app.post("/", function (req, res) {
+  try {
+    console.log("executing deployment...");
+    exec(
+      "ansible-pull -U git@github.com:alanchu9222/ansible-test.git playbook-test1.yml",
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
         }
         if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
+          console.log(`stderr: ${stderr}`);
+          return;
         }
 
         console.log(`stdout: ${stdout}`);
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
 
-        });
-    } catch (e) {
-        console.log(e);
-    }
-
-    res.json({ received: true });
+  res.json({ received: true });
 });
 
-app.listen(8080, '127.0.0.1');
+app.listen(8080, "127.0.0.1");
